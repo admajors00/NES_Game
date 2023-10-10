68fa9540cd9b08acdd81876cd83d9c7d71d097c1
Andrew Majors <andrew.majors@medna.us>
2023-10-10 08:59:04 -0500

    Status Bar

--- src/controller.s
+++ src/controller.s
@@ -14,7 +14,7 @@ Port_2 = $4017
 
 Port_1_Pressed_Buttons = $F0 ;pressed this frame
 Port_1_Down_Buttons = $F1;     = $31 ;held down
-
+Port_1_Released_Buttons = $F2
 UpdateButtons:
     lda Port_1_Down_Buttons
     tay
@@ -26,13 +26,13 @@ UpdateButtons:
     sta Port_1
 
     @loop:
-    lda Port_1
-    lsr
-    rol Port_1_Down_Buttons
-    bcc @loop
-    tya
-    eor Port_1_Down_Buttons
-    and Port_1_Down_Buttons
-    sta Port_1_Pressed_Buttons
+        lda Port_1
+        lsr
+        rol Port_1_Down_Buttons
+        bcc @loop
+        tya
+        eor Port_1_Down_Buttons
+        and Port_1_Down_Buttons
+        sta Port_1_Pressed_Buttons
     rts
 
