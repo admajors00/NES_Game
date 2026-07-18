2b8ac835a5bd91aedccc245b3fc175fc0b121373
andrew.majors <Andrew.Majors@kratosdefense.com>
2026-07-18 00:39:42 -0500

    Updated RLE compressor to remove status bar from background files

--- Tools/RLE_Compresor.py
+++ Tools/RLE_Compresor.py
@@ -24,7 +24,8 @@ def main():
             at_rle_file = input_file.replace(".bin", "_at.rle")
 
             bg_data, at_data = GetData(input_file)
-            bg_data_T, at_data_T = TransposeData(bg_data, at_data)      
+            bg_data_ = RemoveStatusBar(bg_data)
+            bg_data_T = TransposeBgData(bg_data_)      
             bg_data_comp = CompressData(bg_data_T)
             at_data_comp = CompressData( at_data)
 
@@ -36,28 +37,27 @@ def main():
                 for byte in at_data_comp:
                     f.write(byte)
 
-            bg_input_size  = bg_data.size * bg_data.itemsize
-            bg_output_size = bg_data_comp.size * bg_data_comp.itemsize
+  
 
-            at_input_size  = at_data.size * at_data.itemsize
-            at_output_size = at_data_comp.size * at_data_comp.itemsize
-
-            bg_compression_pct = 1 - (bg_output_size / bg_input_size)
-            at_compression_pct = 1 - (at_output_size / at_input_size)
+            bg_compression_pct = 1 - (bg_data_comp.size / bg_data_.size)
+            at_compression_pct = 1 - (at_data_comp.size / at_data.size)
             print(file)
-            print( f"\t bg in: {bg_input_size:>5} bg out: {bg_output_size:>5}  {bg_compression_pct * 100:>6.1f}%")
-            print( f"\t at in: {at_input_size:>5} at out: {at_output_size:>5}  {at_compression_pct * 100:>6.1f}%")
-            total_comp_unop_size += bg_output_size + at_output_size
-            if bg_compression_pct < 0:
-                total_comp_size += bg_input_size
-            else:
-                total_comp_size += bg_output_size
-            if at_compression_pct < 0:
-                total_comp_size += at_input_size
-            else:
-                total_comp_size += at_output_size
+            print( f"\t bg in: {bg_data_.size:>5} bg out: {bg_data_comp.size:>5}  {bg_compression_pct * 100:>6.1f}%")
+            print( f"\t at in: {at_data.size:>5} at out: {at_data_comp.size:>5}  {at_compression_pct * 100:>6.1f}%")
+            
 
-            total_uncomp_size += bg_input_size + at_input_size
+            if bg_compression_pct < 0:
+                total_comp_size += bg_data_.size
+            else:
+                total_comp_size += bg_data_comp.size
+                
+            if at_compression_pct < 0:
+                total_comp_size += at_data.size
+            else:
+                total_comp_size += at_data_comp.size 
+
+            total_comp_unop_size += bg_data_comp.size + at_data_comp.size
+            total_uncomp_size += bg_data.size + at_data.size
 
     compression_pct = 1 - (total_comp_size / total_uncomp_size)
     print(f"Total size:      {total_uncomp_size}")
@@ -69,7 +69,37 @@ def main():
     print(f"Bytes Saved:  {total_uncomp_size - total_comp_size}")
     return
 
+def RemoveStatusBar(bg_data):
+    col = 0
+    row = 0
+    statusBar = np.zeros((6, 32), dtype=np.uint8)
+    cwd = os.getcwd()
+    sb_fileName = cwd + "\graphics\Backgrounds\Statusbar.bin"
+    with open(sb_fileName, 'rb') as f:
+        while (byte := f.read(1)):      
+            statusBar[row, col] = byte[0]
+            col += 1
+            if col >= 32:
+                col = 0
+                row += 1
 
+    col = 0
+    row = 0
+    statusBar2 = np.zeros((6, 32), dtype=np.uint8)            
+    sb2_fileName = cwd + "\graphics\Backgrounds\Statusbar2.bin"
+    with open(sb2_fileName, 'rb') as f:
+        while (byte := f.read(1)):      
+            statusBar2[row, col] = byte[0]
+            col += 1
+            if col >= 32:
+                col = 0
+                row += 1            
+    bg_data_sb = bg_data[0:6]
+    bg_data_nsb = bg_data[6:]
+    if(np.array_equal(statusBar,bg_data_sb ) or np.array_equal(statusBar2,bg_data_sb )):
+        return bg_data_nsb
+    else:
+        return bg_data
 
 
 def GetData(input_file):
@@ -91,19 +121,26 @@ def GetData(input_file):
 
 
 
-def TransposeData( bg_data, attribute_data):
+def TransposeBgData( bg_data):
+    if bg_data.size == 960:
+        bg_data_T = np.zeros((32, 30), dtype=np.uint8)
+        for i in range(0, 30):
+            for j in range(0,32):
+                bg_data_T[j, i] =  bg_data[ i, j]     
+    elif bg_data.size == 768: 
+        bg_data_T = np.zeros((32, 24), dtype=np.uint8)
+        for i in range(0, 24):
+            for j in range(0,32):
+                bg_data_T[j, i] =  bg_data[ i, j]     
+    return bg_data_T
 
-    bg_data_T = np.zeros((32, 30), dtype=np.uint8)
+
+
+def TransposeAtData(attribute_data):
     attribute_data_T = np.zeros((8,8), dtype=np.uint8)
-
-    for i in range(0, 30):
-        for j in range(0,32):
-            bg_data_T[j, i] =  bg_data[ i, j]     
-                    
     for i in range(0,64):
         attribute_data_T[i%8, math.floor(i/8)] = attribute_data[math.floor(i/32), i%32]
-
-    return bg_data_T, attribute_data_T
+    return attribute_data_T
         
 
 
